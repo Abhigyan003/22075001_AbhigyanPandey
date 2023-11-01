25c0277544e5cb0ad3607a5d41df83d4f3dbe034
abhigyan_pandey <abhi8803@pop-os.localdomain>
2023-11-02 00:42:13 +0530

    Hello GitHub

---
+++ url_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns =    [
+    path('', views.index, name='index'),
+    path('get_url', views.url_get, name='get_url'),
+    path('<str:pk>', views.go, name='go')
+]   
